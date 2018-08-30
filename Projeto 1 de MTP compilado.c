4bf675fa7519978cc77025388bfeb579aebd808e
yaricanavar <42750719+yaricanavar@users.noreply.github.com>
2018-08-30 10:10:57 -0300

    Add files via upload

---
+++ Projeto 1 de MTP compilado.c
@@ -0,0 +1,16 @@
+/* P0.c */
+ /*Mariana Soares Carneiro*/ 
+ /*11811EBI012*/ 
+#include <stdio.h> 
+int main(int argc, char ** argv) {
+	int i; 
+	if(argc > 1) 
+	{
+	   if(argc > 2) 
+	   for(i = 0; i < argv[2][0]-'0'; i++) 
+	   printf("Valeu, %s\n", argv[1]); 
+	   else printf("Saudacoes, %s\n", argv[1]);
+	} 
+	else printf("Ola!\n"); 
+	return 0; }
+
